0d51f46422a93dbb64717e0d7c9bbb897b448497
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-21 15:29:14 +0000

    ejercicios

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,6 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
-
+	var divC = document.createElement("div");
+	divC.innerHTML = "Hello World";
+	divC.style.background = "yellow";
+	document.body.appendChild(divC);
 });
--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	var liS = document.createElement("li");
+	liS.innerHTML = "Hello World";
+	let ul = document.querySelector("#myList");
+	ul.appendChild(liS);
 });
