8d7ea508c6194400eac7166d111aa111b47717f2
MartinCoimbra <martin.coimbra.2017@gmail.com>
2021-04-21 23:32:13 +0000

    ejerciciosDOM

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,8 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
-	//your code here
+    //your code here
+	let ul = document.querySelector("#myList");
 	var liS = document.createElement("li");
 	liS.innerHTML = "Hello World";
-	let ul = document.querySelector("#myList");
 	ul.appendChild(liS);
 });
--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,6 +1,13 @@
 window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
+	var select = document.querySelector("#mySelect");
 
-  // your code here
+	// your code here
+	countries.forEach(element => {
+		var option = document.createElement("option");
+		option.append(element);
+		select.appendChild(option);
+    });
+    
 };
